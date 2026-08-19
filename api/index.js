67b9ecc93c50442ff814c327b1cbecbfca33e792
Ismail <ismail@packaging.local>
2026-08-19 13:10:02 +0500

    Fix Vercel handler execution premature exit

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import server from '../server/server.js';
 
-export default function handler(req, res) {
-  server.emit('request', req, res);
+export default async function handler(req, res) {
+  return new Promise((resolve, reject) => {
+    res.on('finish', resolve);
+    res.on('error', reject);
+    server.emit('request', req, res);
+  });
 }
